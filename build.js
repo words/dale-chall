7e6dbae09c57cf3bdceb368ebbfe66f98163714e
Titus Wormer <tituswormer@gmail.com>
2016-11-05 13:50:31 +0100

    Rewrite build script

---
+++ build.js
@@ -0,0 +1,41 @@
+/**
+ * @author Titus Wormer
+ * @copyright 2014 Titus Wormer
+ * @license MIT
+ * @module dale-chall:script
+ * @fileoverview Generate a dictionary of entity names to replacements.
+ */
+
+'use strict';
+
+/* Dependencies. */
+var fs = require('fs');
+var bail = require('bail');
+var jsdom = require('jsdom');
+
+jsdom.env('http://www.readabilityformulas.com/articles/dale-chall-readability-word-list.php', done);
+
+function done(err, window) {
+  var values;
+
+  bail(err);
+
+  values = [].slice.call(window.document.querySelectorAll('table table table td'))
+    .slice(0, -1)
+    .map(function (node) {
+      return node.textContent;
+    })
+    .join(' ')
+    .trim()
+    .split(/\s+/g)
+    .filter(Boolean)
+    .map(function (value) {
+      return value.toLowerCase();
+    })
+    .sort()
+    .filter(function (value, index, all) {
+      return all.indexOf(value, index + 1) === -1;
+    });
+
+  fs.writeFile('index.json', JSON.stringify(values, 0, 2) + '\n', bail);
+}
