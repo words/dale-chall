7bdafc1e0532ea6a60687ce0f346c6dc414f39a7
Titus Wormer <tituswormer@gmail.com>
2017-07-08 23:10:54 +0200

    Update build script

--- build.js
+++ build.js
@@ -1,31 +1,33 @@
 'use strict';
 
 var fs = require('fs');
+var http = require('http');
 var bail = require('bail');
-var jsdom = require('jsdom');
+var concat = require('concat-stream');
+var unified = require('unified');
+var parse = require('rehype-parse');
+var selectAll = require('hast-util-select').selectAll;
+var toString = require('hast-util-to-string');
 
-jsdom.env('http://www.readabilityformulas.com/articles/dale-chall-readability-word-list.php', done);
+http.get('http://www.readabilityformulas.com/articles/dale-chall-readability-word-list.php', function (res) {
+  res.pipe(concat(onconcat)).on('error', bail);
 
-function done(err, window) {
-  var values;
+  function onconcat(buf) {
+    var tree = unified().use(parse).parse(buf);
+    var values = selectAll('.main_container_table_three td', tree)
+      .map(toString)
+      .join(' ')
+      .trim()
+      .split(/\s+/g)
+      .filter(Boolean)
+      .map(function (value) {
+        return value.toLowerCase();
+      })
+      .sort()
+      .filter(function (value, index, all) {
+        return all.indexOf(value, index + 1) === -1;
+      });
 
-  bail(err);
-
-  values = [].slice.call(window.document.querySelectorAll('.main_container_table_three td'))
-    .map(function (node) {
-      return node.textContent;
-    })
-    .join(' ')
-    .trim()
-    .split(/\s+/g)
-    .filter(Boolean)
-    .map(function (value) {
-      return value.toLowerCase();
-    })
-    .sort()
-    .filter(function (value, index, all) {
-      return all.indexOf(value, index + 1) === -1;
-    });
-
-  fs.writeFile('index.json', JSON.stringify(values, 0, 2) + '\n', bail);
-}
+    fs.writeFile('index.json', JSON.stringify(values, 0, 2) + '\n', bail);
+  }
+});
