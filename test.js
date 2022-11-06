de0dfa1b5e164d836f35f18a50e5d957e1711934
Titus Wormer <tituswormer@gmail.com>
2022-11-06 14:43:20 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,10 +1,13 @@
-import test from 'tape'
+import assert from 'node:assert'
+import test from 'node:test'
 import {daleChall} from './index.js'
 
-test('characterEntities', function (t) {
-  t.equal(typeof daleChall, 'object', 'should be an array #1')
-  t.equal(Array.isArray(daleChall), true, 'should be an array #2')
-  t.notEqual(daleChall.indexOf('old-fashioned'), -1, 'should contain words')
-
-  t.end()
+test('characterEntities', function () {
+  assert.equal(typeof daleChall, 'object', 'should be an array #1')
+  assert.equal(Array.isArray(daleChall), true, 'should be an array #2')
+  assert.notEqual(
+    daleChall.indexOf('old-fashioned'),
+    -1,
+    'should contain words'
+  )
 })
